888b2faafb5f01cb1be2a22025c3d68d1a4db08e
Marcus Willock <crazcalm@gmail.com>
2017-04-12 09:17:49 -0400

    Trying to figure out how to best structure the program. So far, I just figured out that I can pass a game/board object to all of the drawn elements, which will allow me to call game methods from all objects within their Tick functions.
    I think that this will allow me the greatest flexibility when I
    start created the over arching game rules

---
+++ game.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	tl "github.com/JoelOtter/termloop"
+)
+
+//Board to the game
+type Board struct {
+	Hieght int
+	Width  int
+	Grid []*Clickable
+}
+
+
+//Clickable ... add docs
+type Clickable struct {
+	*tl.Rectangle
+	Grid *Board
+}
+
+//Tick ... add docs
+func (c *Clickable) Tick (ev tl.Event) {
+	x, y := c.Position()
+	if ev.Type == tl.EventMouse && ev.MouseX == x && ev.MouseY == y {
+		c.SetColor(tl.ColorRed)
+	}
+}
+
+//NewClickable ... add docs
+func NewClickable(x, y, w, h int, col tl.Attr, b *Board) *Clickable {
+	return &Clickable{tl.NewRectangle(x, y, w, h, col,), b}
+}
+
+
+/*
+I need to add a bar of colors that allow me to change
+the current game color so that I can test out clicking
+on the grid with different colors.
+*/
+func main(){
+	white := tl.ColorWhite
+	hieght := 10
+	width := 20
+	grid := []*Clickable{}
+	board := Board{hieght, width, grid}
+	g := tl.NewGame()
+
+	// Used to created the board
+	var tempt *Clickable
+	for w:=0; w < board.Width; w++{
+		for h:=0; h < board.Hieght; h++{
+			tempt = NewClickable(h, w, 1, 1, white, &board)
+			g.Screen().AddEntity(tempt)
+			board.Grid = append(board.Grid, tempt)
+		}
+	}
+	g.Start()
+}
